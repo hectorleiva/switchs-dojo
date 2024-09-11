b5ad4284452758d9e25f9c86ce4e95d9c24f1085
Hector Leiva <hector@hectorleiva.com>
2024-09-10 23:45:53 -0400

    problem for next week, fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+// Multiples of 3: Fizz
+// Multiples of 5: Buzz
+// Multiples of 3 and 5: FizzBuzz
+// Print every other number that
+// doesn't match the above statements
+// from 1 to 100
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 == 0) {
+    console.log("Fizz");
+  }
+}
--- math.js
+++ math.js
@@ -1,49 +1,58 @@
 function add(x, y) {
-    const firstNumber = x;
-    const secondNumber = y;
-    const total = firstNumber + secondNumber;
-    return total;
+  const firstNumber = x;
+  const secondNumber = y;
+  const total = firstNumber + secondNumber;
+  return total;
 }
 
-add(2, 5)
+add(2, 5);
 
 function subtract(x, y) {
-    const firstnumber = x;
-    const secondNumber = y;
-    const total = firstnumber - secondNumber;
-    return total;
+  const firstnumber = x;
+  const secondNumber = y;
+  const total = firstnumber - secondNumber;
+  return total;
 }
 
-subtract(10,9)
+subtract(10, 9);
 
-function multiply (x, y) {
-	const firstnumber = x;
-	const secondnumber = y;
-	const total = firstnumber * secondnumber;
-	return total;
+function multiply(x, y) {
+  const firstnumber = x;
+  const secondnumber = y;
+  const total = firstnumber * secondnumber;
+  return total;
 }
 
-function divide (x, y) {
-	const firstnumber = x;
-	const secondnumber = y;
-	const total = firstnumber / secondnumber;
-	return total;
+function divide(x, y) {
+  const firstnumber = x;
+  const secondnumber = y;
+  const total = firstnumber / secondnumber;
+  return total;
 }
 
-console.log(
-    subtract(
-        add(
-            subtract(200,1),
-            subtract(3,1)
-        ), 1
-    )
-)
-console.log(
-subtract(99,9)
-)
-console.log(
-multiply(5,5)
-)
-console.log(
-divide(20,10)
-)
+const subtractionResultOne = subtract(add(subtract(200, 1), subtract(3, 1)), 1);
+const subtractionResultTwo = subtract(99, 9);
+const multiplicationResult = multiply(5, 5);
+const divisionResult = divide(20, 10);
+const divisionResultOne = divide(20, 20);
+const divisionResultTwo = divide(20, 20);
+const multiplicationResultTwo = multiply(5, 25);
+
+const mathResults = [
+  subtractionResultOne, // 200
+  subtractionResultTwo, // 90
+  multiplicationResult, // 25
+  divisionResult,
+  divisionResultOne,
+  divisionResultTwo,
+  multiplicationResultTwo,
+];
+
+mathResults.forEach(function printer(calculation) {
+  console.log(`${calculation}\n;`);
+});
+
+// console.log(
+//   `${subtractionResultOne}\n;\n${subtractionResultTwo}\n;\n${multiplicationResult}\n;\n${divisionResult}`,
+// );
+
