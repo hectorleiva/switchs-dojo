9e397e38999ce4e0e26d93458b13c5f0be67bbe4
aqcalhoun <aqcalhoun@gmail.com>
2024-09-11 19:39:02 -0700

    updated fizzbuzz with explanations and solved conditions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,8 +5,24 @@
 // doesn't match the above statements
 // from 1 to 100
 
-for (let i = 0; i <= 100; i++) {
-  if (i % 3 == 0) {
-    console.log("Fizz");
+//set parameters, establish starting number, increase count until it reaches 100
+for (let i = 1; i <= 100; i++) {
+//if number is divisible by both 3 and also 5, display "Fizzbuzz"
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");	
+  }
+// else if number is divisble by 5, then display "Buzz"
+  else if (i % 5 == 0) {
+	  console.log("Buzz");
+	  
+  }
+// else if number is divisble by 3, then display "Fizz"
+  else if (i % 3 == 0) {
+	  console.log("Fizz");
+	  
+  } 
+// else if number is not divisible by 3 and also 5, then print number as usual
+  else if (i % 3 !== 0 && i % 5 !== 0) {
+	  (console.log (i));
   }
 }
