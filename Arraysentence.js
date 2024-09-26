d46a729b47c34ec46a1b95a3f19d1e3298ff51f3
aqcalhoun <aqcalhoun@gmail.com>
2024-09-25 18:37:24 -0700

    added Arraysentence and array numbers files/homework

---
+++ Arraysentence.js
@@ -0,0 +1,4 @@
+let simplesentence = [`the`,`quick`,`fox`,`jumps`,`over`,`the`,`lazy`,`dog`];
+
+console.log(simplesentence[0],simplesentence[7],simplesentence[3],simplesentence[4],simplesentence[0],simplesentence[2]);
+
---
+++ arraynumbers.js
@@ -0,0 +1,3 @@
+let arraynumbers = [1,5,9,10,21];
+
+console.log(arraynumbers[4] - arraynumbers[3] + arraynumbers[2] - arraynumbers[1] - arraynumbers[0]);
