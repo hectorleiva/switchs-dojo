df6e9c81e6561fb64f38b84604d07cdfe601c21b
aqcalhoun <aqcalhoun@gmail.com>
2025-06-04 18:23:01 -0700

    uploading branch with current code that isn't complete but created Orders.js file.

--- Node-Practice/app.js
+++ Node-Practice/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var ordersRouter = require("./routes/orders");
 const db = require("./db");
 
 async function connectToDatabase() {
@@ -42,7 +43,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
+app.use("/orders", ordersRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
